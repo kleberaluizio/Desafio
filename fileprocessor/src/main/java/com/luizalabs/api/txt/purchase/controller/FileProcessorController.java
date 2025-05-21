ced09fd30726d1d0b7ec08db9ca93ade9ddea8db
kleberaluizio <kleberaluizio@gmail.com>
2025-05-21 19:02:17 -0300

    Feat - Adiciona anotações do swagger para o endpoit de processamento de arquivo

--- fileprocessor/src/main/java/com/luizalabs/api/txt/purchase/controller/FileProcessorController.java
+++ fileprocessor/src/main/java/com/luizalabs/api/txt/purchase/controller/FileProcessorController.java
@@ -6,6 +6,11 @@ import com.luizalabs.api.txt.purchase.domain.Purchase;
 import com.luizalabs.api.txt.purchase.exception.InvalidFileEntriesException;
 import com.luizalabs.api.txt.purchase.exception.InvalidFilterParameterFormatException;
 import com.luizalabs.api.txt.purchase.service.FileProcessorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collection;
 
+@Tag(name = "File Processing (.txt)")
 @RestController
 @RequestMapping("api/v1/file-processing")
 public class FileProcessorController {
@@ -27,11 +33,23 @@ public class FileProcessorController {
         this.service = service;
     }
 
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Processes a .txt file containing purchase records and returns a JSON with normalized data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful processing"),
+            @ApiResponse(responseCode = "400", description = "Invalid filter parameter format | Invalid entries found in the file")
+    })
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> process(
+            @Parameter(description = "File to be processed (.txt)", required = true)
             @RequestParam("file") MultipartFile file,
+
+            @Parameter(description = "Order ID (type: integer, optional)")
             @RequestParam(value="order_id", required = false) String orderIdStr,
+
+            @Parameter(description = "Start date (format: YYYY-MM-DD, optional)")
             @RequestParam(value="start_date", required = false) String startDateStr,
+
+            @Parameter(description = "End date (format: YYYY-MM-DD, optional)")
             @RequestParam(value="end_date", required = false) String endDateStr)
     {
         try {
